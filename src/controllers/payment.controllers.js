13b9e82d5f2ef7ff5a7736f2ba74d30d39c1f8ae
Lucas Cabral <lucassimple@hotmail.com>
2023-10-24 15:54:34 -0300

    Puliendo detalles 1.0

--- src/controllers/payment.controllers.js
+++ src/controllers/payment.controllers.js
@@ -46,8 +46,8 @@ export const createSession = async (req, res) => {
             mode: "payment",
             // success_url: `http://localhost:${PORT}/success`,
             // cancel_url: `http://localhost:${PORT}/error`
-            success_url: `http://back-end-desafio1-lucascabral-production.up.railway.app//success`,
-            cancel_url: `http://back-end-desafio1-lucascabral-production.up.railway.app//error`
+            success_url: `http://back-end-desafio1-lucascabral-production.up.railway.app/success`,
+            cancel_url: `http://back-end-desafio1-lucascabral-production.up.railway.app/error`
         });
 
 
