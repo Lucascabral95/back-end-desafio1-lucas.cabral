22d45cb5aa43c903888c19493a2ae06cbf29dbe3
Lucas Cabral <lucassimple@hotmail.com>
2023-10-25 15:38:38 -0300

    Ultimando detalles 22.0

--- src/jwt.js
+++ src/jwt.js
@@ -10,9 +10,26 @@ export const generateToken = (user) => {
 }
 
 // MIDDLEWARE QUE RECIBE EL VALOR DE "REQ.COOKIES.AUTHTOKEN" CON EL TOKEN "JWT" Y SE ENCARGA DE LEERLO Y VERIFICAR QUE SEA CORRECTO PARA DEJARTE AVANZAR A LA RUTA DONDE SE LO APLICA.
+// export const authToken = (req, res, next) => {
+//     const token = req.cookies.authToken
+//     if (!token) {
+//         return res.status(401).json({
+//             error: "No tiene token",
+//             message: "No tiene token"
+//         })
+//     }
+//     try {
+//         req.user = jwt.verify(token, KEY_SECRET)
+//         next()
+//     } catch (error) {
+//         return res.status(401).json({
+//             error: "Token invalido.",
+//             message: "El token enviado no es valido o no tiene el nivel de acceso suficiente para este recurso."
+//         })
+//     }
+// }
 export const authToken = (req, res, next) => {
     const token = req.cookies.authToken
-
     if (!token) {
         return res.status(401).json({
             error: "No tiene token",
